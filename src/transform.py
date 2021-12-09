930eb86c200b0c153c524fb990f5e69581f9c574
Johan Schuijt <johan.schuijt@ftm.nl>
2021-12-09 14:25:40 +0100

    make it possible to store most relations through mapping

--- src/transform.py
+++ src/transform.py
@@ -4,48 +4,52 @@ from os import environ
 from ftmstore import get_dataset
 from followthemoney import model
 from followthemoney.cli.util import load_mapping_file
-import yaml
-import pandas as pd
-
-# entities = {
-        # 'entities': {
-            # 'mapping': 'entities.yml'
-        # }
-
-# def load_relations():
-    # columns = ['_start', '_end', '_type', 'link', 'start_date', 'end_date']
-    # dtype = {
-            # 'link': 'str',
-            # 'start_date': 'str',
-            # 'end_date': 'str'
-            # }
-    # return pd.read_csv('data/relationships.csv', usecols=columns, dtype=dtype)
 
 entities = {};
 
-mappings = [
-        'entities.yml',
-        'officers.yml',
-        ]
+mappings = {
+        'entities': 'entities.yml',
+        'officers': 'officers.yml',
+        'addresses': 'addresses.yml',
+        }
+
+def get_map_generators(key, file):
+    config = load_mapping_file('mapping/' + file)
+    for mapping in config[key]['queries']:
+        mapping['csv_url'] = 'file://' + path.abspath(mapping['csv_url'])
+        yield model.make_mapping(mapping)
 
 def main():
     dataset = get_dataset('icij', database_uri=environ.get('DATASTORE_URI'))
     bulk = dataset.bulk()
 
-    for mapping in mappings:
-        mapping = load_mapping_file('mapping/' + mapping)
-        mapping['csv_url'] = 'file://' + path.abspath(mapping['csv_url'])
-        gen = model.make_mapping(mapping)
+    # Process all addresses/entities/officers into corresponding FTM entities through FTM mappings
+    for key, file in mappings.items():
+        for gen in get_map_generators(key, file):
+            for record in gen.source.records:
+                for entity in gen.map(record).values():
+                    # Keep a map of ICIJ _id <=> FTM id
+                    if record['_id'] not in entities:
+                        entities[record['_id']] = entity.id
+                    else:
+                        raise Exception('_id %s is not unique!' % record['_id'])
 
+                    bulk.put(entity)
+
+    # Now process releationships, converting ICIJ _id fields into corresponding FTM entity IDs
+    for gen in get_map_generators('relations', 'relations.yml'):
         for record in gen.source.records:
-            for entity in gen.map(record).values():
-                # Keep a map of ICIJ _id <=> FTM id
-                if record['_id'] not in entities:
-                    entities[record['_id']] = entity.id
-                else:
-                    raise Exception('_id %s is not unique!' % record['_id'])
+            try:
+                record['_start'] = entities[record['_start']]
+                record['_end'] = entities[record['_end']]
 
-                bulk.put(entity)
+                for entity in gen.map(record).values():
+                    bulk.put(entity)
+            except KeyError as err:
+                print('Entity with ID %s not found for %s relation' % (err, record['_type']))
+                continue
+
+    # TODO - Process address relations seperately as they have to be added to entities
 
     bulk.flush()
 
