ec576c35b52ac251f02ec3e8eb3de080a3ea03ee
Johan Schuijt <johan.schuijt@ftm.nl>
2021-12-13 10:32:21 +0100

    add address mapping, add address entities to companies

--- src/transform.py
+++ src/transform.py
@@ -5,16 +5,6 @@ from ftmstore import get_dataset
 from followthemoney import model
 from followthemoney.cli.util import load_mapping_file
 
-entities = {};
-
-mappings = {
-        'entities': 'entities.yml',
-        'officers': 'officers.yml',
-        'intermediaries': 'intermediaries.yml',
-        'others': 'others.yml',
-        # TODO - Add these addresses to related LegalEntity address & county fields
-        # 'addresses': 'addresses.yml',
-        }
 
 def get_map_generators(key, file):
     config = load_mapping_file('mapping/' + file)
@@ -23,21 +13,32 @@ def get_map_generators(key, file):
         yield model.make_mapping(mapping)
 
 def main():
+    mappings = {
+            'entities': 'entities.yml',
+            'officers': 'officers.yml',
+            'intermediaries': 'intermediaries.yml',
+            'others': 'others.yml',
+            'addresses': 'addresses.yml',
+            }
     dataset = get_dataset('icij', database_uri=environ.get('DATASTORE_URI'))
-    bulk = dataset.bulk()
+    entities = {};
+    addresses = {}
 
     # Process all addresses/entities/officers into corresponding FTM entities through FTM mappings
     for key, file in mappings.items():
         for gen in get_map_generators(key, file):
+            bulk = dataset.bulk()
             for record in gen.source.records:
                 for entity in gen.map(record).values():
                     # Keep a map of ICIJ _id <=> FTM id
                     entities[record['_id']] = entity.id
                     bulk.put(entity)
+            bulk.flush()
 
 
     # Now process releationships, converting ICIJ _id fields into corresponding FTM entity IDs
     for gen in get_map_generators('relations', 'relations.yml'):
+        bulk = dataset.bulk()
         for record in gen.source.records:
             try:
                 record['_start'] = entities[record['_start']]
@@ -48,12 +49,20 @@ def main():
             except KeyError as err:
                 print('Entity with ID %s not found for %s relation' % (err, record['_type']))
                 continue
+        bulk.flush()
 
-    bulk.flush()
-
-    # TODO - Process address relations seperately as they have to be added to entities
-    # Use address relation mapping to add address entities to related LegalEntities
-
+    # Add addresses to related LegalEntity addressEntity field
+    for gen in get_map_generators('address-relations', 'address-relations.yml'):
+        bulk = dataset.bulk()
+        for record in gen.source.records:
+            # TODO - Use summary in relation record for address entity
+            company = dataset.get(entities[record['_start']]);
+            address = dataset.get(entities[record['_end']]);
+            address.add('summary', record['link'])
+            bulk.put(address)
+            company.add('addressEntity', address)
+            bulk.put(company)
+        bulk.flush()
 
 if __name__ == "__main__":
     main()
